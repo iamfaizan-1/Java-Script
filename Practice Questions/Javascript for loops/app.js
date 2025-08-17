ab869afcb2fdc8c3dd590308ed9013a305eb4543
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-08-17 11:06:06 -0700

    practice

--- Practice Questions/Javascript for loops/app.js
+++ Practice Questions/Javascript for loops/app.js
@@ -33,9 +33,38 @@
 
 //printing numbers using nested for loops
 
-for (i = 1; i <= 100; i += 10 ){
-    for (j = i ;j < i+10; j++ ){
-        document.write(j + " " );
-    }
-    document.write("</br>");
-}
+// for (i = 1; i <= 100; i += 10 ){
+//     for (j = i ;j < i+10; j++ ){
+//         document.write(j + " " );
+//     }
+//     document.write("</br>");
+// }
+
+// let  sum = 0;
+// for(var i = 1; i<=5 ; i++){
+
+//     sum += i;
+// }
+
+// console.log(sum);
+
+
+// for(var i = 100; i<=200 ; i+=10){
+
+//     for(j = i; j <= i+10; j++ ){
+//         document.write(j+ " " );
+//     }
+//     document.write("</br>")
+// }
+
+
+// for(var i = 0; i<=100; i+=2){
+//     document.write("</br>" +i)
+// }
+
+// for(var i = 0; i<=100; i++){
+//     if(i%2 == 0){
+//         document.write(i + "</br>" )
+//     }
+// }
+
