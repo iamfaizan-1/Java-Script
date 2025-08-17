ddb27b223497d457abfa143a6f0064d1c716a044
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-08-16 22:38:42 -0700

    practice

---
+++ Practice Questions/functions/app.js
@@ -0,0 +1,38 @@
+// function fullName(){
+    
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+
+// var name = firstName+lastName;
+
+// document.write("The name is: ",name);
+
+// }
+
+// fullName()
+
+
+
+
+
+// parameters
+
+
+function fullName(firstName,LastName){
+
+
+var name = firstName+ " "+LastName;
+return name;
+
+}
+
+
+var student1 = fullName("Muhammad","Faizan");
+var student2 = fullName("Abdul","Rehman");
+
+
+console.log("student 1 is: ",student1);
+console.log("Student 2 is: ",student2)
+
+
+
--- Practice Questions/practice folder/app.js
+++ Practice Questions/practice folder/app.js
@@ -1,3 +1,67 @@
-var num1 = 5;
-var num2 = 5;
-alert(num1/num2)
+// var num1 = 5;
+// var num2 = 5;
+// alert(num1/num2)
+
+// increment and decrement
+
+
+
+
+// concatenation
+
+var a = 1;
+var b = "2";
+result = a+b;
+console.log(typeof(result));
+
+
+// cleanest cities
+// var flag = false;
+// var cleanestCities = ["Copenhegen","Santa Fe", "Tucson","Great Falls"];
+
+// function cityCheck(){
+//     var check = document.getElementById("cities").value
+
+// for(var i = 0; i<= 4; i++){
+
+// if(check.toLowerCase() == cleanestCities[i].toLowerCase()){
+//     flag = true;
+//     console.log("One of the cleanest cities");
+//     break;
+// }
+
+// }
+
+
+
+// }
+
+// if(flag == false){
+//     console.log("city not found");
+// }
+
+
+// for(i=1; i<=10 ; i++){
+
+// for(j= 1; j<=i; j++)
+// {
+//     document.write(j);
+    
+// }
+// document.write("</br>");
+
+// }
+
+// var table  = prompt("Enter the number");
+
+// for(i=1; i<=10; i++){
+
+//     var result = table*i;
+
+//     document.write(table+" x 1 =" +  result + "</br>")
+
+
+// }
+
+
+
