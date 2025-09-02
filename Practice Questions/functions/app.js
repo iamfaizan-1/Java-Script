219f746f086df35c710ca609f91cd3c8661feae8
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-09-02 08:00:40 -0700

    functions

--- Practice Questions/functions/app.js
+++ Practice Questions/functions/app.js
@@ -18,21 +18,73 @@
 // parameters
 
 
-function fullName(firstName,LastName){
+// function fullName(firstName,LastName){
 
 
-var name = firstName+ " "+LastName;
-return name;
+// var name = firstName+ " "+LastName;
+// return name;
+
+// }
+
+
+// var student1 = fullName("Muhammad","Faizan");
+// var student2 = fullName("Abdul","Rehman");
+
+
+// console.log("student 1 is: ",student1);
+// console.log("Student 2 is: ",student2)
+
+
+// function without return
+
+// function getSum(x,y){
+
+// var sum = x+y;
+
+// console.log(sum)
+
+// }
+
+
+// getSum(5,6)
+
+
+
+// function with return;
+
+
+// function getName(firstName,LastName){
+
+
+//     var name = firstName+ " " + LastName;
+
+//     return name;
+
+// }
+
+// let student1 = getName("Muhammad","Faizan");
+
+// console.log(student1)
+
+
+function Vowel(name){
+
+    name = name.toLowerCase()
+var count = 0;
+
+    for(i=0; i<= name.length-1; i++){
+
+        if(name[i] === "a" || name[i] === "e" || name[i] === "i" || name[i] === "o" || name[i] === "u"  ){
+
+            count++;
+           
+
+        }
+    }
+
+    console.log("The word has"+ " "+count + " " +"vowel sounds")
 
 }
 
 
-var student1 = fullName("Muhammad","Faizan");
-var student2 = fullName("Abdul","Rehman");
-
-
-console.log("student 1 is: ",student1);
-console.log("Student 2 is: ",student2)
-
-
-
+Vowel("FAIZAN")
