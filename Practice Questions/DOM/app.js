5aba5e6dbcb0d8cadd4fb899fa43617d98144452
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-09-07 22:03:12 -0700

    class Name

--- Practice Questions/DOM/app.js
+++ Practice Questions/DOM/app.js
@@ -19,3 +19,57 @@
 // list.style.display = "none"}
 
 // }
+
+
+// readmore
+
+
+// function toggle(){
+
+// var para = document.getElementById("secondPara");
+
+// var btn = document.getElementById("button");
+
+// if(para.style.display !=="none"){
+//  para.style.display = "none"
+//     btn.innerText = "Show More"
+    
+    
+// }
+
+// else{
+//    para.style.display = "block";
+//     btn.innerText = "Show less"
+// }
+
+
+
+
+
+// }
+
+
+
+// by class Name
+
+
+// function toggle(){
+
+// var para = document.getElementById("secondPara");
+// var btn = document.getElementById("button")
+
+// if(para.className == "hidden"){
+
+//     para.className = "view";
+//     btn.innerText = "Show less"
+
+// }
+
+// else{
+
+//     para.className = "hidden";
+//     btn.innerText = "show more"
+// }
+
+
+// }
