35a200c72e4cc774d871615642543a43752488c6
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-09-07 21:34:23 -0700

    DOM

---
+++ Practice Questions/DOM/app.js
@@ -0,0 +1,21 @@
+
+// function viewHide(){
+
+// var list = document.getElementById("list");
+// var btn = document.getElementById("button");
+
+// if(list.style.display == "none"){
+
+//     list.style.display = "block";
+//     btn.innerText = "Hide"
+// }
+
+// else{
+
+
+
+// btn.innerText = "View";
+
+// list.style.display = "none"}
+
+// }
