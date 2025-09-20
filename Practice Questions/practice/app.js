848369e3ba541fa7c2eafd584643a2166a4884a0
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-09-19 23:53:40 -0700

    changes

---
+++ Practice Questions/practice/app.js
@@ -0,0 +1,33 @@
+
+
+
+// var text = "123abc";
+
+// console.log(parseInt(text))
+
+
+// var text = 1.0;
+
+// console.log(Number(text));
+
+
+// var text = 12345.4778
+
+// console.log(Math.round(text))
+
+// var text = ["my","name","is","Faizan"];
+
+// console.log(text.slice(0,3))
+
+
+// var text = " my name is faizan my name is faizan"
+
+// var text2 = text.replace(/name/g,"man");
+
+// console.log(text2)
+
+
+var number = Math.ceil(Math.random()*5);
+
+console.log(number)
+
