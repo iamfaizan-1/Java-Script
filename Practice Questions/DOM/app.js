7271614e7854205c5a212783e825adbb9e62644e
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-01-28 19:26:49 +0500

    dom practice

--- Practice Questions/DOM/app.js
+++ Practice Questions/DOM/app.js
@@ -1,75 +1,51 @@
 
-// function viewHide(){
+const button = document.getElementById("btn")
+const image = document.getElementById("image")
+function toggle(){
 
-// var list = document.getElementById("list");
-// var btn = document.getElementById("button");
+if(button.innerText == "Bulb is on"){
+    button.innerText = "Bulb is off"
+image.setAttribute("src","https://media.istockphoto.com/id/502789173/photo/light-bulb-concept.jpg?s=612x612&w=0&k=20&c=vJfSDuFJf_4UsQcUsdMNUfDkkNU6Bf9cTHF-n4-Zwvs=")
 
-// if(list.style.display == "none"){
+image.style.width = "200px"
+image.style.transition = "all 0.5s ease"
 
-//     list.style.display = "block";
-//     btn.innerText = "Hide"
-// }
+}
 
-// else{
+else if(button.innerText == "Bulb is off"){
+    button.innerHTML = "Bulb is on"
 
-
-
-// btn.innerText = "View";
-
-// list.style.display = "none"}
-
-// }
-
-
-// readmore
-
-
-// function toggle(){
-
-// var para = document.getElementById("secondPara");
-
-// var btn = document.getElementById("button");
-
-// if(para.style.display !=="none"){
-//  para.style.display = "none"
-//     btn.innerText = "Show More"
-    
-    
-// }
-
-// else{
-//    para.style.display = "block";
-//     btn.innerText = "Show less"
-// }
+    image.setAttribute("src","https://www.shutterstock.com/image-photo/warm-colored-active-bulb-on-260nw-782351350.jpg")
+}
 
 
 
+}
 
 
-// }
+// to do items
+
+const addBtn = document.getElementById("addbtn")
+const MainDiv = document.getElementById("items")
+const input = document.getElementById("input-1")
+addBtn.addEventListener("click",()=>{
+
+
+MainDiv.innerHTML = `<div>
+<ul class="list-group">
+  <li class="list-group-item d-flex justify-content-between ">
+<h3>${input.value}</h3>
+<button class="ms-auto" >Edit</button>
+<button>Remove</button>
+  </li>
+  
+</ul>
+
+</div>`
 
 
 
-// by class Name
+})
 
 
-// function toggle(){
 
-// var para = document.getElementById("secondPara");
-// var btn = document.getElementById("button")
-
-// if(para.className == "hidden"){
-
-//     para.className = "view";
-//     btn.innerText = "Show less"
-
-// }
-
-// else{
-
-//     para.className = "hidden";
-//     btn.innerText = "show more"
-// }
-
-
-// }
