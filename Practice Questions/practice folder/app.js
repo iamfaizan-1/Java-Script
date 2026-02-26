2b6cd43e6481b3f8c0ea9b51d795c8af7df310a6
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-02-26 15:56:05 +0500

    practicing object

--- Practice Questions/practice folder/app.js
+++ Practice Questions/practice folder/app.js
@@ -353,43 +353,379 @@
 // tellTime()
 
 
-var Students ={
-    fullName:"Muhammad Faizan",
-    FatherName:"Deen Muhammad",
-    Department:"Computer Science",
-    Semester:5,
+// var Students ={
+//     fullName:"Muhammad Faizan",
+//     FatherName:"Deen Muhammad",
+//     Department:"Computer Science",
+//     Semester:5,
 
 
-    detail: function(){
-        return this;
+//     detail: function(){
+//         return this;
+//     }
+
+// }
+
+
+// console.log(Students.detail());
+
+
+
+// function GetName()
+
+// {
+//     const name = "faizan"
+
+
+//     return this.name;
+// }
+
+// console.log(GetName())
+
+
+
+const obj = {
+
+}
+
+
+
+var cars ={
+ honda:{
+        city:{
+            carName:"Honda city",
+            model:"2024",
+            fuelType:"Petrol",
+            engine:"6000 RPM",
+            seatCapacity:"5 seater",
+            fuelTankCapacity:"40 L",
+            image:"assets/car.jpg"
+        },
+        accord: {
+            carName: "Honda Accord",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L",
+            seatCapacity: "5",
+            fuelTankCapacity: "56 L",
+            image:"img'/honda accord.png"
+        },
+         civic: {
+            carName: "Honda Civic",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L",
+            seatCapacity: "5",
+            fuelTankCapacity: "47 L",
+            image:"img'/honda civic.png"
+         },
+         crv: {
+            carName: "Honda CR-V",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.4L",
+            seatCapacity: "5",
+            fuelTankCapacity: "57 L",
+            image: "img'/honda crv.png"
+        },
+        jazz: {
+            carName: "Honda Jazz",
+            model: "2024",
+            fuelType: "Petrol",
+            engine: "1.2L",
+            seatCapacity: "5",
+            fuelTankCapacity: "40 L",
+            image: "img'/honda jazz.png"
+        },
+        hr_v: {
+            carName: "Honda HR-V",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.8L",
+            seatCapacity: "5",
+            fuelTankCapacity: "50 L",
+            image: "img'/honda hrv.png"
+        },
+         pilot: {
+            carName: "Honda Pilot",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L",
+            seatCapacity: "7",
+            fuelTankCapacity: "74 L",
+            image: "img'/honda pilot.png"
+        },
+         odyssey: {
+            carName: "Honda Odyssey",
+            model: "2024",
+            fuelType: "Petrol",
+            engine: "3.5L",
+            seatCapacity: "7",
+            fuelTankCapacity: "68 L",
+            image: "img'/honda odssey.png"
+        },
+        ridgeline: {
+            carName: "Honda Ridgeline",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L",
+            seatCapacity: "5",
+            fuelTankCapacity: "73 L",
+            image: "img'/honda ridgelinne.png"
+        },
+         fit: {
+            carName: "Honda Fit",
+            model: "2024",
+            fuelType: "Petrol",
+            engine: "1.5L",
+            seatCapacity: "5",
+            fuelTankCapacity: "40 L",
+            image: "img'/honda fit.png"
+         }
+},
+
+
+ suzuki:{
+        
+        swift: {
+            carName: "Suzuki Swift",
+            model:"2023",
+            fuelType: "Petrol",
+            engine: "1.2L",
+            seatCapacity: "5",
+            fuelTankCapacity: "37 L",
+            image: "img'/suzuki swift.png"
+                
+            },
+        ciaz: {
+            carName: "Suzuki Ciaz",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L",
+            seatCapacity: "5",
+            fuelTankCapacity: "45 L",
+            image: "img'/suzuki ciaz.png"
+
+        },
+        baleno: {
+            carName: "Suzuki Baleno",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.2L",
+            seatCapacity: "5",
+            fuelTankCapacity: "37 L",
+            image: "img'/suzuki baleno.png"
+        },
+        dzire: {
+            carName: "Suzuki Dzire",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.2L",
+            seatCapacity: "5",
+            fuelTankCapacity: "37 L",
+            image: "img'/suz desire.png"
+        },
+        ertiga: {
+            carName: "Suzuki Ertiga",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L",
+            seatCapacity: "7",
+            fuelTankCapacity: "45 L",
+            image: "img'/suzzuki ertiga.png"
+        },
+        vitara: {
+            carName: "Suzuki Vitara",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.6L",
+            seatCapacity: "5",
+            fuelTankCapacity: "47 L",
+            image: "img'/suz vitara.png"
+        },
+        jimny: {
+            carName: "Suzuki Jimny",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L",
+            seatCapacity: "4",
+            fuelTankCapacity: "40 L",
+            image: "img'/suzuki jimny.png"
+        },
+        alto: {
+            carName: "Suzuki Alto",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "0.8L",
+            seatCapacity: "4",
+            fuelTankCapacity: "35 L",
+            image: "img'/suzuki alto.png"
+        },
+        ignis: {
+            carName: "Suzuki Ignis",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.2L",
+            seatCapacity: "5",
+            fuelTankCapacity: "32 L",
+            image: "img'/suzuki ignis.png"
+        },
+        xl7: {
+            carName: "Suzuki XL7",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L",
+            seatCapacity: "7",
+            fuelTankCapacity: "45 L",
+            image: "img'/suzuki xl7.png"
+        }
+    }, 
+
+toyota:{
+    corolla: {
+        carName: "Toyota Corolla",
+        model: "2023",
+        fuelType: "Petrol",
+        engine: "1.8L",
+        seatCapacity: "5",
+        fuelTankCapacity: "50 L",
+        image: "img'/t corolla.png"
+    },
+    camry: {
+        carName: "Toyota Camry",
+        model: "2023",
+        fuelType: "Petrol",
+        engine: "2.5L",
+        seatCapacity: "5",
+        fuelTankCapacity: "60 L",
+        image: "img'/t camry.png"
+    },
+    rav4: {
+        carName: "Toyota RAV4",
+        model: "2023",
+        fuelType: "Petrol",
+        engine: "2.5L",
+        seatCapacity: "5",
+        fuelTankCapacity: "55 L",
+        image: "img'/t rav 4.png"
+    },
+    hilux: {
+        carName: "Toyota Hilux",
+        model: "2023",
+        fuelType: "Diesel",
+        engine: "2.8L",
+        seatCapacity: "5",
+        fuelTankCapacity: "80 L",
+        image: "img'/t hilux.png"
+    },
+    yaris: {
+        carName: "Toyota Yaris",
+        model: "2023",
+        fuelType: "Petrol",
+        engine: "1.5L",
+        seatCapacity: "5",
+        fuelTankCapacity: "42 L",
+        image: "img'/t yaris.png"
+    },
+    fortuner: {
+        carName: "Toyota Fortuner",
+        model: "2023",
+        fuelType: "Diesel",
+        engine: "2.8L",
+        seatCapacity: "7",
+        fuelTankCapacity: "80 L",
+        image: "img'/t fortuner.png"
+    },
+    avalon: {
+        carName: "Toyota Avalon",
+        model: "2023",
+        fuelType: "Petrol",
+        engine: "3.5L",
+        seatCapacity: "5",
+        fuelTankCapacity: "60 L",
+        image: "img'/t avalon.png"
+    },
+    supra: {
+        carName: "Toyota Supra",
+        model: "2023",
+        fuelType: "Petrol",
+        engine: "3.0L",
+        seatCapacity: "2",
+        fuelTankCapacity: "52 L",
+        image: "img'/t supra.png"
+    },
+    prius: {
+        carName: "Toyota Prius",
+        model: "2023",
+        fuelType: "Hybrid",
+        engine: "1.8L",
+        seatCapacity: "5",
+        fuelTankCapacity: "43 L",
+        image: "img'/t prius.png"
+    },
+    highlander: {
+        carName: "Toyota Highlander",
+        model: "2023",
+        fuelType: "Petrol",
+        engine: "3.5L",
+        seatCapacity: "7",
+        fuelTankCapacity: "65 L",
+        image: "img'/t highlaner.png"
+    }
+}
+        
+            
     }
 
+
+    let company = document.getElementById("company")
+let model = document.getElementById("model")
+let info = document.getElementById("info")
+
+for(let key in cars){
+    company.innerHTML+=`<option value='${key}'> ${key} </option>`
 }
 
 
-console.log(Students.detail());
 
 
+const selectCompany = ()=>{
+model.innerHTML = `<option value="" disabled selected >Select model</option>`
 
-function GetName()
-
-{
-    const name = "faizan"
-
-
-    return this;
+    for( let key in cars[company.value]){
+        model.innerHTML+=`
+        <option value='${key}'> ${key} </option>
+        `
+    }
 }
 
-console.log(GetName())
+company.addEventListener("change",selectCompany)
 
 
 
 
 
 
+const selectModel=()=>{
 
 
+let mycompany = company.value
+let mymodel = model.value
+    let carDetail = cars[mycompany][mymodel]
 
+    info.innerHTML=`
+    
+    <h3>Car detail</h3>
+    <p><b>model:</b> ${carDetail.model} </p>
+      <p><b>fuelType:</b> ${carDetail.fuelType} </p>
+        <p><b>engine:</b> ${carDetail.engine} </p>
+          <p><b>seatCapacity:</b> ${carDetail.seatCapacity} </p>
+            <p><b>fuelTankCapacity:</b> ${carDetail.fuelTankCapacity} </p>
+          <img src='${carDetail.image}' width='200px' >
+    `
 
+}
+
+model.addEventListener("change",selectModel)
 
 
