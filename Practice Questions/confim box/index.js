833efd3f3089515b99659a6f23f8cc7ed833270d
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-06-06 10:56:15 +0500

    js practice

---
+++ Practice Questions/confim box/index.js
@@ -0,0 +1,14 @@
+
+
+//1. confirm box
+
+// var a = confirm ("Do you like our website?");
+
+// (a)? alert("Thanks") : alert("sorry")
+
+//2.prompt box
+
+var grade = prompt("what is your grade");
+
+(grade>=80)? alert("You are qualified"):alert("You are not qualified")
+
